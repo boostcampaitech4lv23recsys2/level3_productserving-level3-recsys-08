33049d1b449b1dca42f5c1a85e7c623af05147d5
netsus <cwjcsk@naver.com>
2023-01-18 03:57:49 +0900

    [Feat][조원준] inference 수정

--- interaction_model/inference.py
+++ interaction_model/inference.py
@@ -1,9 +1,10 @@
 import annoy
 import pandas as pd
 
-length_of_vector = 20 # item_vector의 크기
+length_of_vector = 20  # item_vector의 크기
 
-def Inference(file_name:str,movieId:int,topk:int):
+
+def Inference(file_name: str, movieId: int, topk: int):
     """movieId를 입력으로 받아 유사도 높은 컨텐츠의 movieId를 반환합니다.
 
     Args:
@@ -14,9 +15,11 @@ def Inference(file_name:str,movieId:int,topk:int):
     Returns:
        result (list): 유사도 높은 아이템의 movieId topk개를 담고 있는 list 입니다.
     """
-    loaded_annoy = annoy.AnnoyIndex(length_of_vector,"angular")
-    loaded_annoy.load(file_name) # Annoy load
+    loaded_annoy = annoy.AnnoyIndex(length_of_vector, "angular")
+    loaded_annoy.load(file_name)  # Annoy load
 
-    neighbour = loaded_annoy.get_nns_by_item(movieId, topk, include_distances=False)[1:] # 첫 번째 인덱스는 입력으로 넣은 movieId 이므로 제외합니다.
+    neighbour = loaded_annoy.get_nns_by_item(movieId, topk, include_distances=False)[
+        1:
+    ]  # 첫 번째 인덱스는 입력으로 넣은 movieId 이므로 제외합니다.
 
     return neighbour
