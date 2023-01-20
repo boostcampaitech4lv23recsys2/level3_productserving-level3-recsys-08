bb43f5b2603e71df54c3c3ff02a5381ee3cd1e7f
netsus <cwjcsk@naver.com>
2023-01-20 15:03:21 +0900

    [Feat][조원준] MVP 서비스 스타일링 및 코드 수정

--- FastAPI/04_combine/main.py
+++ FastAPI/04_combine/main.py
@@ -7,11 +7,17 @@ from PIL import Image
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 import uvicorn
+from fastapi.staticfiles import StaticFiles
 sys.path.append('../..')
 from Utils import user_input_to_recommend
+from pathlib import Path
 
 
 app = FastAPI()
+app.mount(
+    "/css",
+    StaticFiles(directory=Path(__file__).parent.absolute() / "css"),
+)
 templates = Jinja2Templates(directory='./')
 
 
@@ -28,23 +34,28 @@ class user_info(BaseModel):
 
 Info = user_info
 Info.movie_list = [1704, 1721, 2021, 72998, 73141, 91500, 164909]
-templates = Jinja2Templates(directory='./')
+# templates = Jinja2Templates(directory='./')
 
 
 @app.get("/",response_class = HTMLResponse)
 def insert_info_form(request: Request):
     return templates.TemplateResponse('input.html',context = {'request':request})
 
-@app.post("/")
-def insert_info(request: Request, MBTI: str = Form(...), enneagram: str = Form(...)):
+@app.post('/enneagram', response_class = HTMLResponse)
+def insert_enneagram(request: Request, MBTI: str = Form(...)):
+    print(MBTI)
     Info.MBTI = MBTI
-    Info.enneagram = enneagram
+    return templates.TemplateResponse('enneagram.html', context={'request' : request})
+
+@app.post("/result")
+def insert_info(request: Request):
+    enneagram = '1w9'
     Info.movie_list
-    result = user_input_to_recommend(MBTI, enneagram, Info.movie_list)
+    print(Info.MBTI, enneagram)
+    result = user_input_to_recommend(Info.MBTI, enneagram, Info.movie_list)
     cols=['Character','Contents','MBTI','img_src','Enneagram_sim']
     result_list = result[cols].to_dict(orient='records')
     return templates.TemplateResponse('result_bootstrap.html', {"request": request, "data": result_list})
-
 
 # @app.get("/result/detail{character_id}")
 # def show_result_detail(request:Request, character_id:int):
@@ -59,4 +70,4 @@ def insert_info(request: Request, MBTI: str = Form(...), enneagram: str = Form(.
 
 
 if __name__=='__main__':
-    uvicorn.run(app, host="0.0.0.0", port=8001)
+    uvicorn.run("main:app", host="0.0.0.0", port=8001, reload=True)
