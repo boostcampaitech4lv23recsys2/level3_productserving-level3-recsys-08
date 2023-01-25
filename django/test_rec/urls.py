eba3a322e8525d6225348b5eb0d9b07fccb15e77
nahyunlee <awsed2486@gmail.com>
2023-01-25 03:22:14 +0000

    [Feat][이나현] 장고 프론트 페이지 연결

--- django/test_rec/urls.py
+++ django/test_rec/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path, include
+from . import views
 
 app_name = 'test_rec'
 urlpatterns = [
-    # path('/', include('fastapi.urls'), name='fastapi'),
+    path('start_test/', views.start_test, name='start_test'),
+    path('mbti_test/', views.mbti_test, name='mbti_test'),
+    path('enneagram_test/', views.enneagram_test, name='enneagram_test'),
+    path('enneagram_test2', views.enneagram_test2, name='enneagram_test2'),
+    path('enneagram_test3', views.enneagram_test3, name='enneagram_test3'),
+    path('movie_test', views.movie_test, name='movie_test'),
+    path('result_page', views.result_page, name='result_page'),
 ]
--- django/test_rec/views.py
+++ django/test_rec/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+
 
 # Create your views here.
 def index(request):
@@ -7,3 +9,32 @@ def index(request):
         'datetime' : ""
     }
     return render(request, 'index.html', context)
+
+@csrf_exempt
+def start_test(request):
+    return render(request, 'test_rec/main.html')
+
+@csrf_exempt
+def mbti_test(request):
+    return render(request, 'test_rec/mbti.html')
+
+@csrf_exempt
+def enneagram_test(request):
+    return render(request, 'test_rec/enneagram.html')
+
+@csrf_exempt
+def enneagram_test2(request):
+    return render(request, 'test_rec/enneagram2.html')
+
+@csrf_exempt
+def enneagram_test3(request):
+    add_quest_list = [{'three_letter':1,'question':1},{'three_letter':2,'question':2}]
+    return render(request, 'test_rec/enneagram3.html', {'add_quest_list': add_quest_list})
+
+@csrf_exempt
+def movie_test(request):
+    return render(request, 'test_rec/movie.html')
+
+@csrf_exempt
+def result_page(request):
+    return render(request, 'test_rec/result_bootstrap.html')
