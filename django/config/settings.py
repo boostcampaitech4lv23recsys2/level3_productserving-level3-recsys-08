b79b61551abd6068d185c829eef0bca1c3207766
nahyunlee <awsed2486@gmail.com>
2023-01-25 04:46:50 +0000

    [Fix][이나현] .css 적용하기

--- django/config/settings.py
+++ django/config/settings.py
@@ -119,6 +119,9 @@ APPEND_SLASH = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
