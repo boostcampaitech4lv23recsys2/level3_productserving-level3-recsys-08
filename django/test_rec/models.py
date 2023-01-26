9b55c8232b74207db1c8df3aa56e213715b96c73
nahyunlee <awsed2486@gmail.com>
2023-01-26 03:50:09 +0000

    [Feat][이나현] 장고 test_rec 뷰 완료

--- django/test_rec/models.py
+++ django/test_rec/models.py
@@ -1,12 +1,13 @@
 from django.db import models
+import simplejson as json
 
 
 class TmpUser(models.Model):
     MBTI = models.CharField(max_length=10, null=True, blank=True)
     ennear_ans = models.CharField(max_length=10, null=True, blank=True)
     ennear_res = models.CharField(max_length=10, null=True, blank=True)    
-    prefer_movie_id = models.IntegerField(null=True, blank=True)
-    recommended_character_id = models.IntegerField(null=True, blank=True)
+    prefer_movie_id = models.CharField(max_length=10, null=True, blank=True) #int로 안한 이유는 리스트로 담기 위함이다.
+    recommended_character_id = models.CharField(max_length=10, null=True, blank=True)
     create_time = models.DateTimeField()
 
     def __str__(self):
--- django/test_rec/views.py
+++ django/test_rec/views.py
@@ -3,6 +3,7 @@ from django.views.decorators.csrf import csrf_exempt
 from django.utils import timezone
 import simplejson as json
 import pandas as pd
+import numpy
 import pickle
 import random
 
@@ -12,16 +13,28 @@ sys.path.append('..')
 from Utils import user_input_to_recommend
 
 
+def print_userInfo(user):
+    print(f'userid: {user.id}')
+    print(f'MBTI:{user.MBTI}')
+    print(f'ennear_ans:{user.ennear_ans}')
+    print(f'ennear_res:{user.ennear_res}')
+    print(f'prefer_movie_id:{user.prefer_movie_id}')
+    print(f'recommended_character_id:{user.recommended_character_id}')
+
+
 movieId2poster_path='/opt/ml/input/fighting/Utils/Pickle/movieid_to_poster_file.pickle'
 with open(movieId2poster_path,'rb') as f:
     movieId_to_posterfile = pickle.load(f)
+
 
 ch2mv_path='/opt/ml/input/fighting/Utils/Pickle/characterId_to_movieId.pickle'
 with open(ch2mv_path,'rb') as f:
     characterId_to_movieId = pickle.load(f)
 
+
 mbti_df = pd.read_pickle('/opt/ml/input/fighting/Utils/Pickle/MBTI_merge_movieLens_3229_movie.pickle')
 movie_genre_plot = pd.read_csv('/opt/ml/input/fighting/Data/DataProcessing/movie_genre_plot.csv')
+
 
 @csrf_exempt
 def start_test(request):
@@ -57,6 +70,7 @@ def enneagram_test2(request):
         user.save()
     return render(request, 'test_rec/enneagram2.html')
 
+
 @csrf_exempt
 def enneagram_test3(request):
     user = TmpUser.objects.get(id=request.session['user_id'])
@@ -72,6 +86,7 @@ def enneagram_test3(request):
     add_quest_list = add_quest.to_dict(orient='records')
     print(user.ennear_ans)
     return render(request, 'test_rec/enneagram3.html', {'add_quest_list': add_quest_list})
+
 
 @csrf_exempt
 def movie_test(request):
@@ -96,15 +111,21 @@ def result_page(request):
     user = TmpUser.objects.get(id=request.session['user_id'])
     if request.method == 'POST':
         movies = request.POST.getlist('movies')
-        movie_list = [int(i.split('_')[0]) for i in movies]
+        movie_list = [i.split('_')[0] for i in movies]
         user.prefer_movie_id = json.dumps(movie_list)
-
+        
+        movie_list = [int(i) for i in movie_list]
         result = user_input_to_recommend(user.MBTI, user.ennear_res, movie_list)
         result = result[result.Enneagram_sim.notna()]
-        # user.save()
-    # print(f'userid: {user.id}')
-    # print(f'MBTI:{user.MBTI}')
-    # print(f'ennear_ans:{user.ennear_ans}')
-    # print(f'ennear_res:{user.ennear_res}')
-    # print(f'prefer_movie_id:{user.prefer_movie_id}')
-    return render(request, 'test_rec/result_bootstrap.html')
+        result.Enneagram_sim = result.Enneagram_sim.map(lambda x: int(round(x*100)))
+        
+        character_list = result['CharacterId'].values.tolist()
+        character_list = [str(i) for i in character_list]
+        user.recommended_character_id = json.dumps(character_list)
+        user.save()
+
+        cols=['CharacterId','Character','Contents','MBTI','img_src','Enneagram_sim']
+        result_list = result[cols].to_dict(orient='records')
+        context = {"data": result_list}
+
+        return render(request, 'test_rec/result_bootstrap.html', context)
