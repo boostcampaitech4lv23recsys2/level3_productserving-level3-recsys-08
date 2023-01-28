00f32f2a9a2769d25a8819018c884f9a4d2fc725
jeonhaelee <jhealee0624@gmail.com>
2023-01-28 02:12:19 +0000

    [Feat][전해리] inter_3229 영화 랜덤 추출

--- Data/DataProcessing/movie_select.py
+++ Data/DataProcessing/movie_select.py
@@ -3,7 +3,7 @@ import numpy as np
 from sklearn import cluster 
 from sklearn import preprocessing
 
-### movie_select() 함수에서 필요한 함수들
+### inter_movie_select() 함수에서 필요한 함수들
 def delete_s(sen):
     sen = sen.replace(" ","")
     return sen
@@ -16,16 +16,16 @@ def delete(genre_sentence):
         genre_sentence = list(map(delete_s, genre_sentence.split(",")))
         return genre_sentence
     except:
-        return ["no_genre"]
+        return None
     
 def get_encoding(genre, encoding, genre_dic):
-    g_encoding = np.zeros(28)
+    g_encoding = np.zeros(len(genre_dic))
     for g in genre:
         g_encoding += encoding[genre_dic[g]]
     return g_encoding
 ###
 
-def movie_select():
+def inter_movie_select():
     """
         유저가 선호하는 영화를 선택하는 페이지에서 사용되는 영화 랜덤 추출 함수입니다.
         처리 내용 : 연도(>=2000), 인터랙션(>=1000), 클러스터링(0 : 6개, 1 : 1개, 2 : 3개)
@@ -33,14 +33,15 @@ def movie_select():
     """
     
     # 데이터 불러오기 - 자료실에서 해당 데이터 다운받아서 사용하시면 됩니다.
-    data_3229 = pd.read_csv("/opt/ml/.jupyter/lab/workspaces/data/3229_plus_side_info_plus_korean_genre_and_plot.csv")
-    ml_inter = pd.read_csv("/opt/ml/.jupyter/lab/workspaces/data/ml-25m/ratings.csv")
+    data_3229 = pd.read_csv("/data/3229_plus_side_info_plus_korean_genre_and_plot.csv")
+    ml_inter = pd.read_csv("/data/ml-25m/ratings.csv")
 
     # 데이터프레임 제작 과정
-    data = data_3229[['Contents', 'Votes', 'movieId', 'genres']]
+    data = data_3229[['Contents', 'movieId', 'genres']]
     data = data.drop_duplicates(subset=['movieId'])
     data['movieId'] = data['movieId'].apply(lambda x : int(x))
     data['genres_list'] = data['genres'].apply(lambda x:delete(x))
+    data.dropna(axis=0, inplace=True)
     
     genre = []
     for genres in list(data['genres_list']):
@@ -61,57 +62,75 @@ def movie_select():
 
     encode_df = pd.merge(data['movieId'], pd.DataFrame(list(data['genre_encode']), index=data['movieId']), on='movieId',how='inner')
     X = encode_df.iloc[:,1:]
+    X = preprocessing.StandardScaler().fit_transform(X)
     
     kmeans = cluster.KMeans(n_clusters=3, random_state=2023)
-    
-    X = preprocessing.StandardScaler().fit_transform(X)
     kmeans.fit(X)
     
     cluster_label = kmeans.labels_ 
-    encode_df['cluster1'] = cluster_label
+    encode_df['cluster'] = cluster_label
     
-    data = pd.merge(data, encode_df[['movieId','cluster1']], on='movieId', how='inner')
+    data = pd.merge(data, encode_df[['movieId','cluster']], on='movieId', how='inner')
     
     inter = pd.DataFrame(ml_inter.groupby('movieId')['userId'].count().reset_index())
     data = pd.merge(data, inter, on='movieId', how='left')
-
+    data.columns = ['Contents', 'movieId', 'genres', 'genres_list', 'genre_encode', 'cluster', 'inter']
+    
     year = data['Contents'].apply(lambda x: x.strip()[-5:-1])
     year_li = []
     for y in year:
         try:
             year_li.append(int(y))
         except:
-            year_li.append(0)
+            year_li.append(None)
     data['year'] = year_li
-    
-    # 인터랙션 >= 1000, 연도 >= 2000 데이터 필터링
-    ml_select = data[(data['userId'] >= 1000) & (data['year'] >= 2000)]
+    data.dropna(axis=0, inplace=True)
+    data['year'] = data['year'].apply(lambda x: int(x))
     
     # 각 군집에서 영화 랜덤 추출
+    data_0 = data.loc[data['cluster'] == 0, :]
+    data_0 = data_0.loc[(data_0['year'] >= 2008) & (data_0['inter'] >= 3000)]
+    
+    data_1 = data.loc[data['cluster'] == 1, :]
+    data_1 = data_1.loc[(data_1['year'] >= 2008) & (data_1['inter'] >= 3000)]
+    
+    data_2 = data.loc[data['cluster'] == 2, :]
+    data_2 = data_2.loc[(data_2['year'] >= 2008) & (data_2['inter'] >= 3000)]
+    
     movieId_list = []
     for i in range(3):
-        if i == 0: n = 6
-        elif i == 1: n = 1
-        elif i == 2: n = 3
-        movieId_list.extend(list(ml_select[ml_select['cluster1'] == i].sample(n, replace=False)['movieId']))
+        if i == 0: n = 6; df = data_0
+        elif i == 1: n = 12; df = data_1
+        elif i == 2: n = 2; df = data_2
+        movieId_list.extend(list(df.sample(n, replace=False)['movieId']))
     
     return movieId_list
 
 
-def movie_select_2():
+def inter_movie_select_2():
     """
         위의 movie_select() 함수와 동일한 결과를 얻는 함수입니다.
         다만 연도 처리(2000년 이상 영화), 인터랙션 처리(1000 이상 영화), 클러스터링이 다 끝난 상태로 저장된
         "movie_random_select_3229.csv" 파일을 바로 불러와서 군집별로 추천 결과만 바로 뽑아줍니다.
     """
     
-    select = pd.read_csv("/opt/ml/.jupyter/lab/workspaces/Data/DataProcessing/movie_random_select_3229.csv")
+    data = pd.read_csv("/movie_random_select_3229.csv")
+    
+    # 각 군집에서 영화 랜덤 추출
+    data_0 = data.loc[data['cluster'] == 0, :]
+    data_0 = data_0.loc[(data_0['year'] >= 2008) & (data_0['inter'] >= 3000)]
+    
+    data_1 = data.loc[data['cluster'] == 1, :]
+    data_1 = data_1.loc[(data_1['year'] >= 2008) & (data_1['inter'] >= 3000)]
+    
+    data_2 = data.loc[data['cluster'] == 2, :]
+    data_2 = data_2.loc[(data_2['year'] >= 2008) & (data_2['inter'] >= 3000)]
     
     movieId_list = []
     for i in range(3):
-        if i == 0: n = 6
-        elif i == 1: n = 1
-        elif i == 2: n = 3
-        movieId_list.extend(list(select[select['cluster1'] == i].sample(n, replace=False)['movieId']))
-        
+        if i == 0: n = 6; df = data_0
+        elif i == 1: n = 12; df = data_1
+        elif i == 2: n = 2; df = data_2
+        movieId_list.extend(list(df.sample(n, replace=False)['movieId']))
+    
     return movieId_list
