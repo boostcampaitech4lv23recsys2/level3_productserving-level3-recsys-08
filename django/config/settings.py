572e1d8fd39a666a26702172493f3f24dd8e3115
ChaeMinsu <chaemiinsu@gmail.com>
2023-01-29 08:39:10 +0000

    [Feat][채민수] GCP Database 연결

--- django/config/settings.py
+++ django/config/settings.py
@@ -112,10 +112,17 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+			'ENGINE'    : 'django.db.backends.mysql',
+			'NAME'      : 'django',
+			'USER'      : 'root',
+			'PASSWORD'  : '',
+			'HOST'      : 'cloudsql/django',
+		}
 }
 
 
