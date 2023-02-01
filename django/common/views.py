bb140ecbed520bc0ca02ba9b841dd7677a874e96
nahyunlee <awsed2486@gmail.com>
2023-02-01 20:38:13 +0000

    [Fix][이나현] 유저 프로필 화면 오류 해결

--- django/common/views.py
+++ django/common/views.py
@@ -36,7 +36,7 @@ def check_TmpUserInfo(request):
 
 pickle_path = Path(__file__).parent.parent.parent.absolute()/"Utils/Pickle"
 movieId2poster_path = pickle_path / 'movieid_to_poster_file.pickle'
-mbti_df = pd.read_pickle(pickle_path / 'MBTI_merge_movieLens_3229_movie.pickle')
+character_df = pd.read_pickle(pickle_path / '230130_Popular_movie_character_2867_cwj.pickle')
 
 
 with open(movieId2poster_path,'rb') as f:
@@ -92,6 +92,8 @@ def user_test_history(request):
 def user_profile(request):
     user = request.user
     tmpusers = TmpUser.objects.filter(LoginUser=user)
+    if len(tmpusers) == 0:
+        return redirect('index')
     mbti = tmpusers[len(tmpusers)-1].MBTI
     # prefer_movie_ids와 해당 포스터 파일이름 불러오는 과정
     tmp = [tmpuser.prefer_movie_id for tmpuser in tmpusers]
@@ -102,7 +104,9 @@ def user_profile(request):
     tmp = [tmpuser.recommended_character_id for tmpuser in tmpusers]
     recommended_character_ids = [eval(tmp[i]) for i in range(len(tmp))]
     recommended_character_ids = [item for sublist in recommended_character_ids for item in sublist]
-    character_images = [mbti_df[mbti_df['CharacterId']==int(id)]['img_src'].values[0] for id in recommended_character_ids]
+    character_images = [character_df[character_df['CharacterId']==int(id)]['img_src'].values[0] \
+                        for id in recommended_character_ids \
+                        if len(character_df[character_df['CharacterId']==int(id)]['img_src']) > 0 ]
     # 템플릿에 넘겨줄 context
     context = {
         'user' : user.username,
