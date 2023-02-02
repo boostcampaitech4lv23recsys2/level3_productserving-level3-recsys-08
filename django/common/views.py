d69d0636531f8225dd1036d5c8e1f4981594e970
jeonhaelee <jhealee0624@gmail.com>
2023-02-02 11:07:48 +0000

    [Feat][전해리] 인덱스 페이지 - 인기있는 캐릭터 Top 10 수정

--- django/common/views.py
+++ django/common/views.py
@@ -73,7 +73,18 @@ def index(request):
     context = {
         'my_person_list': [],
         'datetime' : "",
-        'characters' : []
+        'characters' : [
+                        "https://static1.personality-database.com/profile_images/10372ac29c714ea8aa84fdfccfd9ae8e.png",\
+                        "https://static1.personality-database.com/profile_images/fc179ba7fe644eaa82a1aca584e16868.png",\
+                        "https://static1.personality-database.com/profile_images/ec0fdef9370245c69e9547daf3eff906.png",\
+                        "https://static1.personality-database.com/profile_images/1d86aef46ec14549b24000306bc36db9.png",\
+                        "https://static1.personality-database.com/profile_images/3bcb54ca72024d8a9c00dae8712009f0.png",\
+                        "https://static1.personality-database.com/profile_images/f614546e3d5e434c98b695fe7735a98a.png",\
+                        "https://static1.personality-database.com/profile_images/3877aed32c3b4d7185b22eabd80b9939.png",\
+                        "https://static1.personality-database.com/profile_images/c207665f45f14fb2b9ccc78554e68790.png",\
+                        "https://static1.personality-database.com/profile_images/be417e9fdf2e4604a564d6ceaa1b6b28.png",\
+                        "https://static1.personality-database.com/profile_images/0087da2072a14eec8d01e541e9d9e98f.png",\
+                        ]
     }
     return render(request, 'index.html', context)
 
