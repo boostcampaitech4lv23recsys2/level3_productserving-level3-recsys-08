d59d9ae10dd83a11ae962ceb492a720f22292eea
nahyunlee <awsed2486@gmail.com>
2023-02-04 10:12:13 +0000

    [Feat][이나현] tmpuser 상세 페이지

--- django/common/urls.py
+++ django/common/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(next_page="/"), name='logout'),
     path('signup/', views.signup, name='signup'),
     path('user_profile/', views.user_profile, name='user_profile'),
+    path('detail_tmpuser/<int:tmpuser_id>/', views.detail_tmpuser, name='detail_tmpuser'),
     path('delete_tmpuser/<int:tmpuser_id>/', views.delete_tmpuser, name='delete_tmpuser'),
     path('show_mbti_info/<str:mbti>/', views.show_mbti_info, name='show_mbti_info'),
 ]
