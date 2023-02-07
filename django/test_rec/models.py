178af9e34662d38cbabaa9f85dead23029ab9918
netsus <cwjcsk@naver.com>
2023-02-08 04:51:09 +0900

    [Feat][조원준] 어노이, bts 비율에 따른 추천 결과 좋아요 싫어요 기록

--- django/test_rec/models.py
+++ django/test_rec/models.py
@@ -10,10 +10,14 @@ class TmpUser(models.Model):
     ennea_ans2 = models.CharField(max_length=10, null=True, blank=True)
     ennea_res = models.CharField(max_length=10, null=True, blank=True)    
     prefer_movie_id = models.TextField(null=True, blank=True) #int로 안한 이유는 리스트로 담기 위함이다.
+    annoy_recommend_movies = models.TextField(null=True, blank=True)
+    annoy_bts_ratio = models.CharField(max_length=10, null=True, blank=True)
+    bts_recommend_movies = models.TextField(null=True, blank=True)
     recommended_character_id = models.TextField(null=True, blank=True)
     fit_character_id = models.TextField(null=True, blank=True)
     recommended_character_sim = models.TextField(null=True, blank=True)
     fit_character_sim = models.TextField(null=True, blank=True)
+    feedback = models.IntegerField(default=0)
     create_time = models.DateTimeField()
 
     def __str__(self):
